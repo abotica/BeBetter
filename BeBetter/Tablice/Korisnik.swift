5cd6f4bcac57b5d136a04160031bf180261f0fe1
abotica <andrija.botica1613@icloud.com>
2023-12-14 15:58:14 +0100

    Popravak "Korisnik.swift"

--- BeBetter/Tablice/Korisnik.swift
+++ BeBetter/Tablice/Korisnik.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct Korisnik: Encodable{
+    var id: UUID?
+    var name: String
+    var surname: String
+    var username: String
+    var email: String
+    var password: String
+    var confirmPassword: String
+}

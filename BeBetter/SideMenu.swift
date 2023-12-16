13823ad32dd3a54d056c9a585ca75c15977ba1cb
abotica <andrija.botica1613@icloud.com>
2023-12-16 22:23:12 +0100

    Nastavak karlovih postavki stable

--- BeBetter/SideMenu.swift
+++ BeBetter/SideMenu.swift
@@ -10,11 +10,18 @@ import SwiftUI
 struct SideMenu: View {
     @State var Screen_Width = Register_screen(goBack: .constant(false), removeLoginView: .constant(false), isRegistered: .constant(false), isShownOnce: .constant(true)).Screen_Width
     @State var Screen_Height = Register_screen(goBack: .constant(false), removeLoginView: .constant(false), isRegistered: .constant(false), isShownOnce: .constant(true)).Screen_Height
+    
+    @State var showSettingsView: Bool = false
+    
     var body: some View {
         
             VStack{
                 Button(action: {
                     
+                    //sa animacijon trigera tamo u nekon if-u da se pokaže view
+                    withAnimation(.linear(duration: 0.5)){
+                        showSettingsView.toggle()
+                    }
                     
                 }, label: {
                     HStack{
@@ -43,26 +50,6 @@ struct SideMenu: View {
                             .foregroundStyle(Color.accentColorInvert)
                         
                         Text("Log out")
-                            .font(.custom("Ketibeh-Regular", size: 27))
-                            .frame(width: 100, height: 100)
-                            .foregroundStyle(Color.accentColorInvert)
-                        
-                    }
-                })
-                
-                Divider()
-                    .frame(width: 150, height: 2)
-                    .background(LinearGradient(colors: [Color(#colorLiteral(red: 0.9139844775, green: 0.767064631, blue: 0.4150085449, alpha: 1)), Color(#colorLiteral(red: 0.9025250077, green: 0.4324684143, blue: 0.3178541958, alpha: 1))], startPoint: .topLeading, endPoint: .bottomTrailing))
-                
-                Button(action: {
-                    
-                    
-                }, label: {
-                    HStack{
-                        Image(systemName: "hand.thumbsup.fill").font(.largeTitle).bold()
-                            .foregroundStyle(Color.accentColorInvert)
-                        
-                        Text("Rate our app")
                             .font(.custom("Ketibeh-Regular", size: 27))
                             .frame(width: 100, height: 100)
                             .foregroundStyle(Color.accentColorInvert)
